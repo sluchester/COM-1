cd70b69be93b11dd241fd0b62cc19e01cd716511
Luan de Barros <luanbarrossantos@gmail.com>
2024-09-28 14:05:49 -0300

    comentando o código da aula

--- aula2_2309/sum.m
+++ aula2_2309/sum.m
@@ -1,3 +1,6 @@
+# Exemple of page 174 of RTL-SDR Matlab book, showing sometimes is more efficient to see a signal in the
+# frequency domain and not in the time domain.
+
 clear all; close all; clc;
 
 f= 100;
@@ -8,9 +11,17 @@ ts = 1/fs;
 phi = 0;
 
 t = 0 : ts : 1;
-
 s1_t = 10*cos(2*pi*f*t) + cos(2*pi*2*f*t) + 4*cos(2*pi*3*f*t);
 
-figure; hold on; grid on;
+subplot(2,1,1); hold on; grid on;
 plot(t,s1_t);
 xlim([0 5*T]);
+
+X_f = fft(s1_t/length(s1_t));
+X_f = fftshift(X_f);
+
+passo_f = length(t)/(fs+1);
+f_axis = -fs/2 : passo_f : fs/2;
+
+subplot(2,1,2); hold on; grid on;
+plot(f_axis, X_f);
