7313be09b0054b9335e26fe656ca86111cd17bb6
Luan de Barros <luanbarrossantos@gmail.com>
2024-10-07 13:43:56 -0300

    Parte doe filtros

--- lab1/ex2.m
+++ lab1/ex2.m
@@ -1,3 +1,69 @@
 pkg load signal;
 
-clear all; close all; clc;
+clear all; close all; clc;f= 1000;
+T= 1/f;
+
+fs = 60 * f;
+ts = 1/fs;
+phi = 0;
+
+t = 0 : ts : 1;
+
+s1_t = 6*sin(2*pi*f*t);
+s2_t = 2*sin(2*pi*3*f*t);
+s3_t = 4*sin(2*pi*5*f*t);
+
+s_t = s1_t + s2_t + s3_t;
+
+figure;
+subplot(4,1,1); hold on; grid on;
+plot(t, s1_t);
+xlim([0 3*T]);
+
+subplot(4,1,2); hold on; grid on;
+plot(t, s2_t);
+xlim([0 3*T]);
+
+subplot(4,1,3); hold on; grid on;
+plot(t, s3_t);
+xlim([0 3*T]);
+
+subplot(4,1,4); hold on; grid on;
+plot(t, s_t);
+xlim([0 3*T]);
+
+
+% dominio da frequencia
+passo_f = length(t)/(fs+1);
+f_axis = -fs/2 : passo_f : fs/2;
+
+X_fs1 = fft(s1_t)/length(s1_t);
+X_fs1 = fftshift(X_fs1);
+
+X_fs2 = fft(s2_t)/length(s2_t);
+X_fs2 = fftshift(X_fs2);
+
+X_fs3 = fft(s3_t)/length(s3_t);
+X_fs3 = fftshift(X_fs3);
+
+X_fst = fft(s_t)/length(s_t);
+X_fst = fftshift(X_fst);
+
+% falta colocar label
+figure;
+subplot(4,1,1); hold on; grid on;
+plot(f_axis, abs(X_fs1));
+
+subplot(4,1,2); hold on; grid on;
+plot(f_axis, abs(X_fs2));
+
+subplot(4,1,3); hold on; grid on;
+plot(f_axis, abs(X_fs3));
+
+subplot(4,1,4); hold on; grid on;
+plot(f_axis, abs(X_fst));
+
+% filtros
+filtro_pb = 
+filtro_pa = 
+filtro_pf = 
