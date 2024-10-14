c02fe640141e599104af96826fb407ea6e084ca0
Luan de Barros <luanbarrossantos@gmail.com>
2024-10-14 13:40:12 -0300

    Alterações no vetor

--- lab1/ex2.m
+++ lab1/ex2.m
@@ -1,13 +1,15 @@
 pkg load signal;
 
-clear all; close all; clc;f= 1000;
+clear all; close all; clc;
+f= 1000;
 T= 1/f;
 
 fs = 60 * f;
 ts = 1/fs;
 phi = 0;
 
-t = 0 : ts : 1;
+# t = 0 : ts : 1;
+t = 0 : ts : (300001 - 1)*ts;
 
 s1_t = 6*sin(2*pi*f*t);
 s2_t = 2*sin(2*pi*3*f*t);
@@ -34,8 +36,14 @@ xlim([0 3*T]);
 
 
 % dominio da frequencia
-passo_f = length(t)/(fs+1);
-f_axis = -fs/2 : passo_f : fs/2;
+# passo_f = length(t)/(fs+1);
+passo_f = fs / length(t);
+f_axis = -fs/2 : passo_f : fs/2 - passo_f;
+
+% Certifique-se de que o comprimento de f_axis corresponde ao de t
+if length(f_axis) > length(t)
+    f_axis = f_axis(1:end-1); % Remove o último elemento se necessário
+end
 
 X_fs1 = fft(s1_t)/length(s1_t);
 X_fs1 = fftshift(X_fs1);
@@ -63,14 +71,44 @@ plot(f_axis, abs(X_fs3));
 subplot(4,1,4); hold on; grid on;
 plot(f_axis, abs(X_fst));
 
-
-% filtros
+% filtros no domínio do tempo
 filtro_pb = [zeros(1,148000) ones(1,4001) zeros(1,148000)];
 filtro_pa = [zeros(1,154001) ones(1,145999)];
 filtro_pf = [zeros(1,146000) ones(1,2000) zeros(1,4001) ones(1,2000) zeros(1,146000)];
 
-% plotar figura dos filtros no tempo contínuo para verificar como fica e também no domínio da frequência
+st_pb = s_t .* filtro_pb;
+
 figure;
-subplot(4,1,1); hold on; grid on;
-% isso aqui tá errado
-% plot(t, sum(filtro_pb));
+subplot(3,1,1); hold on; grid on;
+plot(t, st_pb);
+
+st_pa = s_t .* filtro_pa;
+subplot(3,1,2); hold on; grid on;
+plot(t, st_pa);
+
+st_pf = s_t .* filtro_pf;
+subplot(3,1,3); hold on; grid on;
+plot(t, st_pf);
+
+% plotar figura dos filtros no tempo contínuo
+# figure;
+# subplot(3,1,1); hold on; grid on;
+# plot(filtro_pb);
+# xlim([146000 154000]);
+# title('Filtro Passa-Baixa');
+# xlabel('Frequência');
+# ylabel('Amplitude');
+
+# subplot(3,1,2); hold on; grid on;
+# plot(filtro_pa);
+# xlim([152000 156000]);
+# title('Filtro Passa-Alta');
+# xlabel('Frequência');
+# ylabel('Amplitude');
+
+# subplot(3,1,3); hold on; grid on;
+# plot(filtro_pf);
+# xlim([144000 156000]);
+# title('Filtro Passa-Alta');
+# xlabel('Frequência');
+# ylabel('Amplitude');
