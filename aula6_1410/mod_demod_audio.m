aba9025abcc49e03372986682ccf1cecf05f679f
Luan de Barros <luanbarrossantos@gmail.com>
2024-10-18 09:14:58 -0300

    Atualizando as infos

--- aula6_1410/mod_demod_audio.m
+++ aula6_1410/mod_demod_audio.m
@@ -4,8 +4,15 @@ pkg load signal;
 
 filename= 'audio_com.wav';
 
+# o novo sinal agora é y
 [y, fs] = audioread(filename);
 
-t = (0:length(y)-1)/fs;
-plot(t,y);
+% periodo de amostragem
+ts = 1/fs;
 
+% vetor de tempo para plotagem
+t = (0:length(y)-1)/fs;
+
+
+
+plot(t,y);
