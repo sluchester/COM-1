627b7d47699f05879768b92de4db84f1882e28bd
Luan de Barros <luanbarrossantos@gmail.com>
2024-10-21 13:36:42 -0300

    Verificar depois ainda

--- lab1/ex3.m
+++ lab1/ex3.m
@@ -11,11 +11,26 @@ ruido = sqrt(var_n)*randn(1, num_amostras); % Gera ruído com 10.000 amostras
 % plotando o ruído no domínio do tempo
 tempo = 0 : T : (duracao - T); % Cria o vetor de tempo com 10.000 pontos
 
-plot(tempo, ruido);
-title('Ruído com Distribuição Normal');
-xlabel('Tempo (s)');
-ylabel('Amplitude');
+% distribuicao normal
+% figure; hold on; grid on;
+% hist(ruido, num_amostras);
 
+% plote do dominio do tempo
+% figure; hold on; grid on;
+% plot(tempo, ruido);
+% title('Ruído com Distribuição Normal');
+% xlabel('Tempo (s)');
+% ylabel('Amplitude');
+
+% plote do dominio da frequencia
+passo_f = length(tempo)/(fs);
+f_axis = -fs/2 : passo_f : fs/2;
+
+Xf_ruido = fft(ruido)/length(ruido);
+Xf_ruido = fftshift(Xf_ruido);
+
+figure; hold on; grid on;
+plot(f_axis, abs(Xf_ruido));
 
 # Pot_ruido = (1/length(ruido))*sum(ruido.^2)
 # Pot_sinal = (1/length(sinal_t))*sum(sinal_t.^2)
